62ce7c1e1529a9026d2b23e5e0ca1ac316a133e1
JefftheCloudDog <87271581+JefftheCloudDog@users.noreply.github.com>
2021-08-01 23:35:13 -0400

    Add files via upload
    Fixed extra parentheses in printed common words
    Made max words to print as a variable
    Allowed user to input website url

--- wikiScrape.py
+++ wikiScrape.py
@@ -4,68 +4,88 @@ from bs4 import BeautifulSoup, Tag
 from collections import Counter
 import nltk
 from nltk.corpus import stopwords
+import unicodedata
+
+words_display = 5
 
 response = requests.get(
-    url="https://en.wikipedia.org/wiki/cat"
+    url=input()
 )
 
-print("Code:", response.status_code)
+# print("Code:", response.status_code)
 
 soup = BeautifulSoup(response.content, 'lxml')
 
 title = soup.find(id="firstHeading")
 print("Article:", title.text)
 
-# cltur k c/u
-headers = soup.select(".mw-headline")
+sections = soup.select_one(".mw-parser-output").findChildren(recursive=False)
+
+for rmv in sections:
+
+    if rmv.has_attr('id') and (rmv['id'] == 'toc'):
+        # print("Removed:",  rmv['id'])
+        sections.remove(rmv)
+        break
 
 
-for sc in headers:
+words = dict()
 
-    if sc.parent.name == "h2":
-        print("\tSection:", sc.text)
-    elif sc.text == "References" or sc.text == "Reference":
-        quit()
+for sect in sections:
+    # exits only if start of new sub(section)
+    if sect.find(class_="mw-headline"):
+        words = dict(
+            sorted(words.items(), key=lambda x: x[1], reverse=True))
+
+        print("\t\tCommon Words:", end=" ")
+        i = 1
+        for value in words:
+            if value not in stopwords.words('english'):
+                print(value, end=" ")
+                i += 1
+                if i > words_display:
+                    print("")
+                    break
+
+        words.clear()
+
+        if sect.text == "References":
+            exit()
+
+    # start of (sub)section
+    # TEMP
+    # if sect.name == "h2":
+    #     exit()
+    # is a section
+    if(sect.find(class_="mw-headline") != None):
+        if sect.name == "h2":
+            print("Section:", sect.text)
+        else:
+            print("\tSub-Section:", sect.text)
     else:
-        print("\t\tSub-Section:", sc.text)
-    sc = sc.parent
-    sc = sc.find_next_sibling()
+        # do word analysis here
+        # print(sect.text.encode("UTF8"))
+        # print(unicodedata.normalize('NFKC', sect.text))
 
-    wordOcur = dict()
-    while sc != None and sc.find(class_="mw-headline") == None:
-        if len(sc.text) > 0:
-            # print("\t\t\t\tText:", sc.text)
-            # do word analysis
+        # fixes text
+        text = sect.text
+        text = text.replace('(', '').replace(')', '')
+        text = text.split()
 
-            text = sc.text
-            text = text.split()
+        # word analysis
+        for word in text:
+            word = word.lower()
+            if word in words:
+                words[word] += 1
+            else:
+                words.update({word: 1})
 
-            for w in text:
-                w = w.lower()
-                if w in wordOcur:
-                    wordOcur[w] += 1
-                else:
-                    wordOcur.update({w: 1})
-
-        for link in sc.find_all('a', {'href': True}, recursive=True):
-            if link["href"][:5] != "#cite":
-                if len(link["href"]) > 5 and "/wiki/" in link["href"][:6].lower():
-                    print("\t\t\t\t\tLink: https://en.wikipedia.org" +
-                          link["href"])
-                else:
-                    print("\t\t\t\t\tLink:", link["href"])
-
-        sc = sc.find_next_sibling()
-
-    wordOcur = dict(
-        sorted(wordOcur.items(), key=lambda x: x[1], reverse=True))
-
-    print("\t\t\tCommon Words:", end=" ")
-    i = 1
-    for value in wordOcur:
-        if value not in stopwords.words('english'):
-            print(value, end=" ")
-            i += 1
-            if i > 3:
-                print("")
-                break
+        links = sect.find_all('a', {'href': True}, recursive=True)
+        if links != None:
+            for link in links:
+                if link["href"][0] != "#" and link["href"][-11:] != "action=edit":
+                    if len(link["href"]) > 5 and "/wiki/" in link["href"][:6].lower():
+                        print("\t\t\t\t\tLink: https://en.wikipedia.org" +
+                              link["href"])
+                    else:
+                        print("\t\t\t\t\tLink:", link["href"])

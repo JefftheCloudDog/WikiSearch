66171767b90e0f93f5fe4f347e967267c00bff5a
JefftheCloudDog <87271581+JefftheCloudDog@users.noreply.github.com>
2021-08-10 13:34:51 -0400

    Code Clean Up
    Cleaned up comments and organized code for less redundancy.
    Moved word dict print inside later equivalent if statement.

--- wikiScrape.py
+++ wikiScrape.py
@@ -1,44 +1,47 @@
-import urllib
 import requests
-from bs4 import BeautifulSoup, Tag
-from collections import Counter
-import nltk
+from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
-import unicodedata
 
-words_display = 5
+words_display = 5  # number of popular words to show
 
+# use this instead for user input for words amount
+# print("Number of words to display:", end=" ")
+# words_display=input()
+
+
+print("Wikipedia Article Link:", end=" ")
 response = requests.get(
     url=input()
 )
-
-# print("Code:", response.status_code)
+print('\n')
 
 soup = BeautifulSoup(response.content, 'lxml')
 
 title = soup.find(id="firstHeading")
 print("Article:", title.text)
 
+# gets every section component
 sections = soup.select_one(".mw-parser-output").findChildren(recursive=False)
 
+# removes Navigation Box elements
 for rmv in sections:
-
     if rmv.has_attr('id') and (rmv['id'] == 'toc'):
-        # print("Removed:",  rmv['id'])
         sections.remove(rmv)
         break
 
-
+# dictionary for storing word occurences per section
 words = dict()
 
 for sect in sections:
-    # exits only if start of new sub(section)
+    # "mw-headline" are section headings
     if sect.find(class_="mw-headline"):
+        # sorts dictionary based on occurrence (value)
         words = dict(
             sorted(words.items(), key=lambda x: x[1], reverse=True))
 
         print("\t\tCommon Words:", end=" ")
         i = 1
+        # prints and dumps words from dictionary because of new section start
         for value in words:
             if value not in stopwords.words('english'):
                 print(value, end=" ")
@@ -49,30 +52,23 @@ for sect in sections:
 
         words.clear()
 
-        if sect.text == "References":
+        # quits on References
+        if sect.find(class_="mw-headline").text == "References":
             exit()
 
-    # start of (sub)section
-    # TEMP
-    # if sect.name == "h2":
-    #     exit()
-    # is a section
-    if(sect.find(class_="mw-headline") != None):
+        text = sect.find(class_="mw-headline").text
         if sect.name == "h2":
-            print("Section:", sect.text)
+            print("Section:", text)
         else:
-            print("\tSub-Section:", sect.text)
+            print("\tSub-Section:", text)
     else:
-        # do word analysis here
-        # print(sect.text.encode("UTF8"))
-        # print(unicodedata.normalize('NFKC', sect.text))
 
-        # fixes text
+        # cleans up text references
         text = sect.text
-        text = text.replace('(', '').replace(')', '')
+        text = text.replace('(', '').replace(')', '').replace(',', '')
         text = text.split()
 
-        # word analysis
+        # updates word occurrences in dictionary
         for word in text:
             word = word.lower()
             if word in words:
@@ -80,11 +76,13 @@ for sect in sections:
             else:
                 words.update({word: 1})
 
+        # find all links and prints
         links = sect.find_all('a', {'href': True}, recursive=True)
         if links != None:
             for link in links:
                 if link["href"][0] != "#" and link["href"][-11:] != "action=edit":
                     if len(link["href"]) > 5 and "/wiki/" in link["href"][:6].lower():
+                        # Refactors Wikipedia article links
                         print("\t\t\t\t\tLink: https://en.wikipedia.org" +
                               link["href"])
                     else:
